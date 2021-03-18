67a814b3e38cdade8c9b03af50407f33cb9ecfdc
cyber_gaz <43444535+cybergaz@users.noreply.github.com>
2021-03-18 10:09:37 +0530

    Update patterns_1.py

--- patterns_1.py
+++ patterns_1.py
@@ -1,8 +1,6 @@
 a = int(input("enter the limit"))
-c = 65
-for x in range(1,a+1):
-	for y in range(0,x):
-		print(chr(c),end=' ')
-	     c=c+1
+for x in range(a):
+	for y in range(65,65+x+1):
+		print(chr(y),end=' ')
 	print()
 
